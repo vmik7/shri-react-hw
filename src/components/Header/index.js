dcaeceb569ee3d47e7b0b8f0dd4815e5ab073b78
vmik7 <vmik7@yandex.ru>
2021-06-17 23:24:05 +0300

    Вёрстка Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
  
+import './style.scss';
+
 import Button from '../generic/Button'
 
 function Header({ title, buttons }) {
     return (
         <header className="header">
-            <h1 className="header__title">{ title }</h1>
-            { buttons.map(button => <Button className="header__button" text={ button.text }/>) }
+            <div className="container header__container">
+                <h1 className="header__title">{ title }</h1>
+                { buttons.map(button => <Button text={ button.text }/>) }
+            </div>
         </header>
     );
 }
