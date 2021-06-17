a6c287fc6ce0ced8330bf513cde75ff670a3e832
vmik7 <vmik7@yandex.ru>
2021-06-18 00:46:49 +0300

    Вёрстка Button и Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,14 @@ import React from 'react';
  
 import './style.scss';
 
-import Button from '../generic/Button'
+import Button from '../generic/Button';
 
 function Header({ title, buttons }) {
     return (
         <header className="header">
             <div className="container header__container">
                 <h1 className="header__title">{ title }</h1>
-                { buttons.map(button => <Button text={ button.text }/>) }
+                { buttons.map(buttonProps => <Button {...buttonProps} classList={['header__control']}/>) }
             </div>
         </header>
     );
