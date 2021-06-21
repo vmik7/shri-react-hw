462cf5e8df84a8d2e54a0acabd93cee00e69a5d8
vmik7 <vmik7@yandex.ru>
2021-06-21 11:50:13 +0300

    Типизация Button

--- src/components/generic/Button/index.tsx
+++ src/components/generic/Button/index.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 
 import './style.scss';
 
-function Button({
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    text: string,
+    hasIcon?: boolean,
+    iconOnly?: boolean,
+    svg?: React.ReactSVGElement,
+    isPrimary?: boolean,
+    isSmall?: boolean,
+    isDisabled?: boolean,
+    classList?: Array<string>,
+    onClick?: (e: React.MouseEvent) => void,
+}
+
+export default function Button({
     text = 'Click me!',
     hasIcon = false,
     iconOnly = false,
-    svg = '',
+    svg,
     isPrimary = false,
     isSmall = false,
     isDisabled = false,
     classList = [],
     onClick,
     ...props
-}) {
+} : ButtonProps) {
+
     let classArray = ['button', ...classList];
     if (isPrimary) {
         classArray.push('button_primary');
@@ -40,5 +53,3 @@ function Button({
         </button>
     );
 }
-
-export default Button;
